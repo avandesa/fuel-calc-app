b80c5f263077f80545767a0e7204eed53cb9730c
Alex van de Sandt <alex@avandesa.dev>
2020-12-27 17:19:31 -0500

    Add twitter and github links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import styles from '../styles/Home.module.css';
 
@@ -15,6 +16,26 @@ export default function Home() {
                 <h1>Fuel Calculator</h1>
                 <Calculator />
             </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://github.com/avandesa/fuel-calc-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src="/img/github.png"
+                        alt="View source code on Github"
+                    />
+                </a>
+                <a
+                    href="https://twitter.com/alexvds17"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src="/img/twitter.png" alt="Follow me on Twitter" />
+                </a>
+            </footer>
         </div>
     );
 }
